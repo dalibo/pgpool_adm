0866fbe6406e03cbdf90235f87031a06ae7130bd
Jehan-Guillaume (ioguix) de Rorthais <jgdr@dalibo.com>
2011-06-09 16:03:08 +0200

    Add some comments

--- pgpool_adm--1.0.sql
+++ pgpool_adm--1.0.sql
@@ -4,23 +4,34 @@
  * Administrative functions for pgPool
  * *********************************************** */
 
+/**
+ * input parameters: node_id, host, timeout, port, username, password
+ */
 CREATE FUNCTION pcp_node_info(integer, text, integer, integer, text, text, OUT host text, OUT port integer, OUT status text, OUT weight float4)
 RETURNS record
 AS 'MODULE_PATHNAME', '_pcp_node_info'
 LANGUAGE C VOLATILE STRICT;
 
+/**
+ * input parameters: node_id, server_name
+ */
 CREATE FUNCTION pcp_node_info(integer, text, OUT host text, OUT port integer, OUT status text, OUT weight float4)
 RETURNS record
 AS 'MODULE_PATHNAME', '_pcp_node_info'
 LANGUAGE C VOLATILE STRICT;
 
+/**
+ * input parameters: host, timeout, port, username, password
+ */
 CREATE FUNCTION pcp_pool_status(text, integer, integer, text, text, OUT item text, OUT value text, OUT description text)
 RETURNS record
 AS 'MODULE_PATHNAME', '_pcp_pool_status'
 LANGUAGE C VOLATILE STRICT;
 
+/**
+ * input parameters: server_name
+ */
 CREATE FUNCTION pcp_pool_status(text, OUT item text, OUT value text, OUT description text)
 RETURNS record
 AS 'MODULE_PATHNAME', '_pcp_pool_status'
 LANGUAGE C VOLATILE STRICT;
-
